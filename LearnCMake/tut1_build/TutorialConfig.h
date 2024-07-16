803beae04468d957dd26a4c097313141bd81b6ca
guzhexuan <zhexuan.gu@connect.polyu.hk>
2024-07-16 22:49:36 +0800

    The first example of CMake. Learn how to write CMakeLists(e.g., version control, executable file and its correspnding source code to compile.). Besides, some tricks like configure header file and set up include header file search directory

---
+++ LearnCMake/tut1_build/TutorialConfig.h
@@ -0,0 +1,16 @@
+/*
+ * @author: Zhexuan Gu
+ * @Date: 2024-07-16 21:11:51
+ * @LastEditTime: 2024-07-16 21:12:07
+ * @FilePath: /CPPprojects/restart_CPP/LearnCMake/tut1/TutorialConfig.h.in
+ * @Description: Please implement
+ */
+
+// the configured options and settings for Tutorial
+#define Tutorial_VERSION_MAJOR 1
+#define Tutorial_VERSION_MINOR 0
+
+/*
+When CMake configures this header file,
+the values for 1 and 0 will be replaced.
+*/
